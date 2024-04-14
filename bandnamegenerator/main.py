c2c7167cf10e7af781e5b7ec604bcf8e188b063e
hinksonAndrew <38588648+hinksonAndrew@users.noreply.github.com>
2024-04-14 17:14:26 -0500

    cleanup of day 1 code

--- bandnamegenerator/main.py
+++ bandnamegenerator/main.py
@@ -1,11 +1,11 @@
-#printing a welcome message to the user
+# printing a welcome message to the user
 print("Welcome to the Band Name Generator.")
 
-#asking user for city they grew up in using input assigned to a variable to use later
+# asking user for city they grew up in using input assigned to a variable to use later
 city = input("What's the name of the city you grew up in?\n")
 
-#asking user for pets name and assigning to a variable to use later
+# asking user for pets name and assigning to a variable to use later
 pets_name = input("What's your pet's name?\n")
 
-#printing a band name using the city and pet name they entered previously
+# printing a band name using the city and pet name they entered previously
 print("Your band name could be " + city + " " + pets_name)
